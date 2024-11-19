68abcc9ea917bb01e9ed36f73f6135d8f1fd6158
Igor <idontsov@seznam.cz>
2024-11-19 19:14:47 +0100

    +

--- insurance_management/pojistenci/urls.py
+++ insurance_management/pojistenci/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_pojisteny, name='add_pojisteny'),
     path('list/', views.list_pojistencu, name='list_pojistencu'),
+    path('', views.search_view, name='home'),
 ]
