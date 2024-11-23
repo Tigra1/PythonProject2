3a75dc5177a985df32bbf830a7f7d196552bf0c3
Tigra1 <idontsov@seznam.cz>
2024-11-23 14:42:43 +0100

    Update manage.py

--- insurance_management/manage.py
+++ insurance_management/manage.py
@@ -16,10 +16,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    port = os.getenv("PORT", "8000")
     sys.argv = ["manage.py", "runserver", f"0.0.0.0:{port}"]
     execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
-    port = os.getenv("PORT", "8000")
+    main()
 
