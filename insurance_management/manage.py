075d0a7c980b7c5bda0f392d74cc86db5907995b
Tigra1 <idontsov@seznam.cz>
2024-11-23 13:21:45 +0100

    Update manage.py

--- insurance_management/manage.py
+++ insurance_management/manage.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
+    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "insurance_management.settings")
-
-    port = os.environ.get("PORT", "8000")  # Используем порт из переменной окружения или значение по умолчанию
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +15,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    sys.argv = ["manage.py", "runserver", f"0.0.0.0:{port}"]
     execute_from_command_line(sys.argv)
 
 
